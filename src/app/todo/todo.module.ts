353a91701c9878514f9ba535deece6db3f7c709c
Lee-Seonghyun316 <mn031621019@gmail.com>
2022-05-03 14:24:20 +0900

    Add: todos 컴포넌트 생성, 바인딩

---
+++ src/app/todo/todo.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {TodosComponent} from './todos/todos.component';
+import { TodoComponent } from './todos/todo/todo.component';
+import {FormsModule} from "@angular/forms";
+
+
+@NgModule({
+  declarations: [
+    TodosComponent,
+    TodoComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+  ],
+  providers: [],
+  exports: [
+    TodosComponent
+  ]
+})
+export class TodoModule {
+}
